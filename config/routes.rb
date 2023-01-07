99ce48f9d1373dfdfc3af3d075c0745675ab5ca4
Jp Fortuno <jp.fortuno@nullpod.com>
2023-01-07 11:39:25 +0400

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "restaurants/new", to: "restaurants#new"
-  post "restaurants", to: "restaurants#create"
-  get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
-  patch "restaurants/:id", to: "restaurants#update"
-  delete "restaurants/:id", to: "restaurants#destroy"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 end
